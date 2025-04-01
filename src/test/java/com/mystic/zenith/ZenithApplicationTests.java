89bce0fca4397652c27b8785b9d6ae581f3bfd05
MyStic <khanh.votuan@hcmut.edu.vn>
2025-04-01 14:14:28 +0700

    init

---
+++ src/test/java/com/mystic/zenith/ZenithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystic.zenith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
